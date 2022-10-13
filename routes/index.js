6647c6f0b44f23a1dd6ed90b3fc531ba41afa384
Clement <perso@clementhurel.fr>
2022-10-13 18:01:06 +0900

    set up views

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
-
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+  res.redirect('/')
+});
 
 module.exports = router;
